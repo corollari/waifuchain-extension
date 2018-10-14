30bd1c83e479c350c1c9518eddc6fae7988f7066
Albert <me@albert.sh>
2018-10-15 01:22:55 +0200

    Add popup

--- extension/bg.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-      chrome.tabs.executeScript(
-          tab.id,
-//	  {code: "alert();"});
-          {"file": "contentScript.js"});
-});
